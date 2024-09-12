cc15387491132266fa7c183354036e40c0147a46
Pin Pin <99624960+Pin99978@users.noreply.github.com>
2024-09-12 12:54:10 +0800

    Create main.sql

---
+++ Problems/183.Customers Who Never Order/main.sql
@@ -0,0 +1,55 @@
+-- Code
+SELECT c.name AS Customers FROM Customers c
+LEFT JOIN Orders o On c.id = o.customerID
+where o.id is NULL;
+
+-- Problem
+/*
+Table: Customers
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+
+Table: Orders
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+
+Write a solution to find all customers who never order anything.
+Return the result table in any order.
+The result format is in the following example.
+
+Example 1:
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+*/
+
