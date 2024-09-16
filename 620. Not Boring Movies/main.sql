7ac132383b0a0ffef85f90238f6c44c19e72e149
Pin Pin <99624960+Pin99978@users.noreply.github.com>
2024-09-16 14:57:34 +0800

    Create main.sql

---
+++ 620. Not Boring Movies/main.sql
@@ -0,0 +1,61 @@
+
+--hint :
+--1. ORDER BY .. DESC  & ORDER BY .. ASEC  
+-- DESC ( high -> low ) , ASEC ( low -> high) SQL default ASEC
+--2. MOD()  
+-- like 77 % 10  = 7 -> MOD(77,7) = 7 
+
+SELECT c.id ,  c.movie , c.description , c.rating FROM Cinema c
+WHERE MOD(c.id , 2) = 1 AND c.description != "boring"
+ORDER BY c.rating DESC;
+
+
+/*
+Table: Cinema
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| id             | int      |
+| movie          | varchar  |
+| description    | varchar  |
+| rating         | float    |
++----------------+----------+
+id is the primary key (column with unique values) for this table.
+Each row contains information about the name of a movie, its genre, and its rating.
+rating is a 2 decimal places float in the range [0, 10]
+ 
+
+Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+
+Return the result table ordered by rating in descending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+Output: 
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
+Explanation: 
+We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
+
+
+*/
