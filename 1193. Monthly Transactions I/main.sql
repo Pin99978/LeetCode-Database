5412d88d3392f6ed3a26399da03e4ade7474659e
Pin Pin <99624960+Pin99978@users.noreply.github.com>
2024-09-22 10:55:56 +0800

    Update main.sql

--- 1193. Monthly Transactions I/main.sql
+++ 1193. Monthly Transactions I/main.sql
@@ -1,13 +1,14 @@
 -- # Write your MySQL query statement below
-SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month,
-country,
-COUNT(amount) AS trans_count,
-SUM(IF(state = 'approved', 1, 0)) AS approved_count,
-SUM(amount) AS trans_total_amount,
-SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
-FROM Transactions
-GROUP BY month, country
 
+SELECT 
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END ) AS approved_total_amount 
+FROM Transactions  
+GROUP BY month , country
 
 
 /*
